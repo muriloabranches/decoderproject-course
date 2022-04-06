959b3441555438be2acdc62e4bc79ca589410cb5
Murilo Abranches <muriloads@hotmail.com>
2022-04-05 23:15:19 -0300

    Filtros Avançados em APIs com Specification e Pageable

--- src/main/java/com/ead/course/services/CourseService.java
+++ src/main/java/com/ead/course/services/CourseService.java
@@ -1,8 +1,11 @@
 package com.ead.course.services;
 
 import com.ead.course.models.CourseModel;
+import com.ead.course.specifications.SpecificationTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,5 +17,5 @@ public interface CourseService {
 
     Optional<CourseModel> findById(UUID courseId);
 
-    List<CourseModel> findAll();
+    Page<CourseModel> findAll(Specification<CourseModel> spec, Pageable pageable);
 }
